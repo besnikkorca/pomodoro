8e938e2e0172b222acbae2c83e6ecc2a6da55c3c
Besnik Korca <besnik.korca1@gmail.com>
2022-10-26 10:46:18 +0200

    set time in seconds and add mutations, actions to use app

--- src/components/PomodoroApp.vue
+++ src/components/PomodoroApp.vue
@@ -1,32 +1,50 @@
 <template>
   <article>
     {{ description }}
-    <div>
+    <form v-on:submit.prevent="start">
       <label for="pause"
-        >Pause
-        <input id="pause" type="number" v-bind:value="pause" v-on:input="setPause" />
+        >Pause:
+        <input
+          id="pause"
+          type="number"
+          v-bind:value="pauseInMinutes"
+          v-on:input="setPauseInMinutes"
+        />
       </label>
       <label for="work">
-        <input id="work" type="number" v-bind:value="work" v-on:input="setWork" />
+        Work:
+        <input id="work" type="number" v-bind:value="workInMinutes" v-on:input="setWorkInMinutes" />
       </label>
-      <button>Test 123</button>
-    </div>
+      <button>Start</button>
+    </form>
   </article>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 @Options({
   name: 'PomodoroApp',
-  computed: mapState(['work', 'pause']),
+  computed: {
+    ...mapState(['work', 'pause']),
+    ...mapGetters(['pauseInMinutes', 'workInMinutes']),
+  },
   methods: {
     setWork(event: Event) {
       this.$store.commit('setWork', (event.target as HTMLInputElement).value);
     },
+    setWorkInMinutes(event: Event) {
+      this.$store.commit('setWorkInMinutes', (event.target as HTMLInputElement).value);
+    },
     setPause(event: Event) {
       this.$store.commit('setPause', (event.target as HTMLInputElement).value);
+    },
+    setPauseInMinutes(event: Event) {
+      this.$store.commit('setPauseInMinutes', (event.target as HTMLInputElement).value);
+    },
+    start() {
+      this.$store.dispatch('start');
     },
   },
   data() {
@@ -37,29 +55,47 @@ import { mapState } from 'vuex';
   },
 })
 export default class PomodoroApp extends Vue {
-  setWork!: (event: InputEvent) => void;
+  setWork!: (event: Event) => void;
 
-  setPause!: () => void;
+  setPause!: (event: Event) => void;
+
+  setWorkInMinutes!: (event: Event) => void;
+
+  setPauseInMinutes!: (event: Event) => void;
+
+  start!: () => void;
 
   description!: string;
 
   pause!: number;
 
   work!: number;
+
+  pauseInMinutes!: number;
+
+  workInMinutes!: number;
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 article {
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-color: rgb(30, 225, 25);
-  max-width: 1280px;
+  max-width: 500px;
   margin: 0 auto;
   padding: 1rem;
   border-radius: 5px;
 }
 
-div {
+#container {
+  display: flex;
+  flex-direction: column;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,42 @@
 import { createStore } from 'vuex';
 
+const SECONDS_IN_MINUTE = 60;
+const FIVE_MINUTES_IN_SECONDS = 5 * SECONDS_IN_MINUTE;
+const TWENTYFIVE_MINUTES_IN_SECONDS = 25 * SECONDS_IN_MINUTE;
+
 export default createStore({
   state: {
-    pause: 5,
-    work: 25,
+    pause: FIVE_MINUTES_IN_SECONDS,
+    work: TWENTYFIVE_MINUTES_IN_SECONDS,
+    timer: 0,
+    session: 'none',
   },
-  getters: {},
+  getters: {
+    pauseInMinutes(state) {
+      return state.pause / SECONDS_IN_MINUTE;
+    },
+    workInMinutes(state) {
+      return state.work / SECONDS_IN_MINUTE;
+    },
+  },
   mutations: {
     setPause(state, payload) {
       state.pause = payload;
     },
+    setPauseInMinutes(state, payload) {
+      state.pause = payload * SECONDS_IN_MINUTE;
+    },
     setWork(state, payload) {
       state.work = payload;
     },
+    setWorkInMinutes(state, payload) {
+      state.work = payload * SECONDS_IN_MINUTE;
+    },
   },
-  actions: {},
+  actions: {
+    start() {
+      console.log('start');
+    },
+  },
   modules: {},
 });
