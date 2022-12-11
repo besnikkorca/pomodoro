0d04f1eb4e14d4e0418627ae68f8055d7b7d3c1a
Besnik Korca <besnik.korca1@gmail.com>
2022-12-11 14:11:43 +0100

    add PomodoroApp comp && implement functionality

--- src/components/PomodoroApp.vue
+++ src/components/PomodoroApp.vue
@@ -1,39 +1,52 @@
 <template>
   <article>
-    {{ description }}
-    <form v-on:submit.prevent="handleStart">
-      <label for="pause"
-        >Pause:
-        <input
-          id="pause"
-          type="number"
-          v-bind:value="pauseInMinutes"
-          v-on:input="setPauseInMinutes"
-        />
-      </label>
-      <label for="work">
-        Work:
-        <input id="work" type="number" v-bind:value="workInMinutes" v-on:input="setWorkInMinutes" />
-      </label>
-      <div v-if="started">{{ session === 'work' ? 'Focus...' : 'Relax...' }}</div>
-      <div>{{ session === 'pause' ? 'Enjoy the couple minutes' : 'Get things done' }}</div>
-      <div v-if="timer">Time left: {{ Math.floor(timer / 60) }}:{{ timer % 60 }}</div>
-      <div v-else>Stoped</div>
-      <button>Start</button>
-      <button type="button" v-on:click="handleStop">stop</button>
+    <p>{{ description }}</p>
+    <form v-on:submit.prevent="onSubmit">
+      <FormInput
+        id="rest"
+        label="Rest"
+        type="number"
+        :value="restInMinutes.toString()"
+        @input="setRestInMinutes($event.target.value)"
+      />
+      <FormInput
+        id="work"
+        label="Work"
+        type="number"
+        :value="workInMinutes.toString()"
+        @input="setWorkInMinutes($event.target.value)"
+      />
+      <Status />
+      <div>
+        <Button v-if="isStopped">Start</Button>
+        <Button v-else type="button" @click="stop">stop</Button>
+      </div>
     </form>
   </article>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import { mapState, mapGetters } from 'vuex';
+import { mapGetters, mapState, mapActions, mapMutations } from '@/store/modules/pomodoro/helpers';
+import { Session } from '@/types/store/pomodoro';
+import { pauseSessions, tickSessions } from '@/constants/pomodoro';
+import FormInput from './Form/FormInput.vue';
+import Button from './Form/Button.vue';
+import Status from './Status.vue';
 
 @Options({
   name: 'PomodoroApp',
+  components: {
+    FormInput,
+    Button,
+    Status,
+  },
   computed: {
-    ...mapState(['session', 'work', 'pause', 'started', 'timer']),
-    ...mapGetters(['pauseInMinutes', 'workInMinutes']),
+    ...mapState(['session']),
+    ...mapGetters(['restInMinutes', 'workInMinutes']),
+    isStopped() {
+      return pauseSessions.includes(this.session);
+    },
   },
   data() {
     return {
@@ -43,79 +56,58 @@ import { mapState, mapGetters } from 'vuex';
     };
   },
   watch: {
-    started(isStarted) {
-      if (isStarted && !this.timeout) {
+    session(newSession) {
+      if (tickSessions.includes(newSession) && !this.timeout) {
         this.timeout = setInterval(() => {
-          this.$store.dispatch('tick');
-        }, 100);
-      } else if (!isStarted && this.timeout) {
+          this.tick();
+        }, 10);
+      } else if (pauseSessions.includes(newSession) && this.timeout) {
         clearInterval(this.timeout);
         this.timeout = null;
       }
     },
   },
   methods: {
-    setWork(event: Event) {
-      this.$store.commit('setWork', (event.target as HTMLInputElement).value);
-    },
-    setWorkInMinutes(event: Event) {
-      this.$store.commit('setWorkInMinutes', (event.target as HTMLInputElement).value);
-    },
-    setPause(event: Event) {
-      this.$store.commit('setPause', (event.target as HTMLInputElement).value);
-    },
-    setPauseInMinutes(event: Event) {
-      this.$store.commit('setPauseInMinutes', (event.target as HTMLInputElement).value);
-    },
-    handleStart() {
-      this.$store.dispatch('start');
-    },
-    handleStop() {
-      this.$store.dispatch('stop');
+    ...mapActions(['start', 'stop', 'tick']),
+    ...mapMutations(['setRestInMinutes', 'setWorkInMinutes']),
+    onSubmit() {
+      if (this.isStopped) return this.start();
+      return this.stop();
     },
   },
 })
 export default class PomodoroApp extends Vue {
-  setWork!: (event: Event) => void;
-
-  setPause!: (event: Event) => void;
-
   setWorkInMinutes!: (event: Event) => void;
 
-  setPauseInMinutes!: (event: Event) => void;
+  setRestInMinutes!: (event: Event) => void;
 
-  handleStart!: () => void;
+  start!: () => void;
 
-  handleStop!: () => void;
+  stop!: () => void;
+
+  isStopped!: boolean;
+
+  onSubmit!: () => void;
 
   description!: string;
 
-  started!: boolean;
+  session!: Session;
 
-  session!: 'none' | 'pause' | 'work';
-
-  timer!: number;
-
-  pause!: number;
-
-  work!: number;
-
-  pauseInMinutes!: number;
+  restInMinutes!: number;
 
   workInMinutes!: number;
 }
 </script>
 
 <style scoped lang="scss">
+p {
+  font-size: 1.2rem;
+}
+
 article {
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: rgb(30, 225, 25);
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 1rem;
-  border-radius: 5px;
 }
 
 #container {
@@ -126,5 +118,6 @@ article {
 form {
   display: flex;
   flex-direction: column;
+  margin: 1rem auto;
 }
 </style>
