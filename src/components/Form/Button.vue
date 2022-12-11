76badb0fb98c4832afe8276ed57e08c3192a92da
Besnik Korca <besnik.korca1@gmail.com>
2022-12-11 14:10:42 +0100

    add Button comp

---
+++ src/components/Form/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :type="type"><slot /></button>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+
+@Options({
+  props: {
+    type: {
+      default: 'submit',
+      type: String,
+    },
+  },
+})
+export default class Button extends Vue {
+  type!: 'button' | 'submit' | 'reset' | undefined;
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.5rem 1.5rem;
+  max-width: 9.375rem;
+  border-radius: 0.5rem;
+  border: none;
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+</style>
