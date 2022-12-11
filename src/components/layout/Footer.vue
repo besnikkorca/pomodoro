26ffd9663344ab9441c2196f8f3f3a225b4f2434
Besnik Korca <besnik.korca1@gmail.com>
2022-12-11 14:11:05 +0100

    add Footer comp

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="foot">
+    <p>Copyright © {{ year }} {{ company }} {{ companyType }}. All rights reserved</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+
+@Options({
+  computed: {
+    company() {
+      return 'BICODE';
+    },
+    companyType() {
+      return 'LLC';
+    },
+    year() {
+      return new Date().getFullYear();
+    },
+  },
+})
+export default class Footer extends Vue {
+  companyType!: string;
+
+  company!: string;
+
+  year!: number;
+}
+</script>
+
+<style lang="scss" scoped>
+.foot {
+  background-color: var(--secondary-color);
+  color: #ffffff;
+  padding: 2rem;
+  box-shadow: 0 0 0.5rem 0.5rem rgba(0, 0, 0, 0.1);
+
+  &__list {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    list-style: none;
+  }
+}
+</style>
